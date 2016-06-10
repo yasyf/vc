840cf623888bb952c6a4ae9ec797a84d98565279
Yasyf Mohamedali <yasyfm@gmail.com>
2016-06-10 02:06:58 -0700

    Fix percentage calculation.

--- app/mailers/vote_mailer.rb
+++ app/mailers/vote_mailer.rb
@@ -2,7 +2,7 @@ class VoteMailer < ApplicationMailer
   def funding_decision_email(to, company)
     @company = company
     @stats = company.stats
-    @yes_percent = company.votes.count > 0 ? @stats[:yes_votes].to_f / company.votes.count : 0
+    @yes_percent = company.votes.final.count > 0 ? @stats[:yes_votes].to_f / company.votes.final.count : 0
     mail to: to, subject: "#{SUBJECT_HEADER} #{company.name} Funding Decision"
   end
 
