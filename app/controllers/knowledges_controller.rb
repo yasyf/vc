335f9393a9a085747cc91a96b4bb3579d4484860
Yasyf Mohamedali <yasyfm@gmail.com>
2016-06-19 02:08:33 -0400

    Sort Knowledges.

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -2,6 +2,6 @@ class KnowledgesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @knowledges = Knowledge.all
+    @knowledges = Knowledge.all.order(created_at: :desc)
   end
 end
