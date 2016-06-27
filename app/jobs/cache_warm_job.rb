8da4936237b6ae10acbe542893b7378b8d4a84e7
Yasyf Mohamedali <yasyfm@gmail.com>
2016-06-27 06:52:58 +0100

    Update CacheWarmJob to destroy companies removed from Trello.

--- app/jobs/cache_warm_job.rb
+++ app/jobs/cache_warm_job.rb
@@ -3,8 +3,12 @@ class CacheWarmJob < ActiveJob::Base
 
   def perform
     Company.all.each do |company|
-      %w(quorum? funded? stats partner_initials).each do |method|
-        company.public_send(method)
+      begin
+        %w(quorum? funded? stats partner_initials).each do |method|
+          company.public_send(method)
+        end
+      rescue Trello::Error => e
+        company.destroy! if e.message =~ /not found/
       end
     end
   end
