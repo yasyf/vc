c53ca9c6b0115452105a38a79bc6de5c44ec567a
Yasyf Mohamedali <yasyfm@gmail.com>
2016-07-31 04:51:37 +0100

    Fix ApplicationMonitorJob when no companies are available.

--- app/jobs/application_monitor_job.rb
+++ app/jobs/application_monitor_job.rb
@@ -4,7 +4,9 @@ class ApplicationMonitorJob < ActiveJob::Base
   queue_as :default
 
   def perform
+    Company.sync!
     companies = List.application.companies
+    return if companies.blank?
     links = companies.map { |company| "<#{company.trello_url}|#{company.name}>" }
     message = "The following companies applied and are waiting to hear back from us!\n#{links.join(', ')}"
     slack_send! ENV['SLACK_CHANNEL'], message, notify: true
