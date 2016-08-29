1c70d1c2afe47a761ed7589c32a630949dd95b48
Yasyf Mohamedali <yasyfm@gmail.com>
2016-08-29 20:48:02 +0100

    Fix validation error on List sync.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
   has_many :companies
 
+  validates :trello_board_id, presence: true
   validates :trello_id, presence: true, uniqueness:true
   validates :name, presence: true
   validates :pos, presence: true, uniqueness: true
@@ -14,7 +15,9 @@ class List < ActiveRecord::Base
   def self.sync!
     Team.for_each do |team|
       Trello::Board.find(team.trello_board_id).lists.each do |list_data|
-        list = List.where(trello_id: list_data.id).first_or_create
+        list = List.where(trello_id: list_data.id).first_or_create do |list|
+          list.trello_board_id = team.trello_board_id
+        end
         list.assign_attributes %w(name pos).map { |p| [p, list_data.public_send(p)] }.to_h
         list.save! if list.changed?
       end
