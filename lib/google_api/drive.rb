c0648c530ec2e2cdd8f8d9a7bf649151d6c61c55
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-01 01:23:22 +0100

    Cache GDrive results

--- lib/google_api/drive.rb
+++ lib/google_api/drive.rb
@@ -2,6 +2,8 @@ require 'google/apis/drive_v3'
 
 module GoogleApi
   class Drive < Base
+    include Concerns::Cacheable
+
     SCOPES = ['https://www.googleapis.com/auth/drive']
 
     def initialize
@@ -10,9 +12,23 @@ module GoogleApi
     end
 
     def find(term, fields = 'files/webViewLink')
+      key_cached({ term: term, fields: fields }) { raw_find(term, fields) }
+    end
+
+    private
+
+    def cache_options
+      { expires_in: jitter(1, :day) }
+    end
+
+    def raw_find(term, fields)
       @drive.list_files(q: "name = '#{term}'", fields: fields).files.first
     rescue Google::Apis::ClientError
       nil
+    end
+
+    def base_cache_key
+      "google_api/drive"
     end
   end
 end
