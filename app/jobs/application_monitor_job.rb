b24b955423bb74f35a6a98cfe5c3679d8784ce18
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-08 13:01:19 +0100

    Fix ApplicationMonitorJob when skipping.

--- app/jobs/application_monitor_job.rb
+++ app/jobs/application_monitor_job.rb
@@ -6,13 +6,15 @@ class ApplicationMonitorJob < ActiveJob::Base
   def perform
     Team.for_each do |team|
       companies = team.lists.application.companies
-      return if companies.blank?
+      next if companies.blank?
       links = if companies.count <= TRELLO_LINK_COUNT_THRESHOLD
         companies.map { |company| "<#{company.trello_url}|#{company.name}>" }
       else
         companies.map(&:name)
       end
-      message = "The following companies applied and are waiting to hear back from us!\n#{links.join(', ')}"
+      message = "The following companies applied and are waiting to hear back from us!" +
+        " If you're already talking with one, please move it to the 'Allocated Point Partner' column." +
+        "\n#{links.join(', ')}"
       team.notify! message
     end
   end
