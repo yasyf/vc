a323fbb0cc550b60dd9b0ec2f2fd4091263b99dd
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-08 12:53:56 +0100

    Prevent trello spam if too many pending companies.

--- app/jobs/application_monitor_job.rb
+++ app/jobs/application_monitor_job.rb
@@ -1,11 +1,17 @@
 class ApplicationMonitorJob < ActiveJob::Base
+  TRELLO_LINK_COUNT_THRESHOLD = 3
+
   queue_as :default
 
   def perform
     Team.for_each do |team|
       companies = team.lists.application.companies
       return if companies.blank?
-      links = companies.map { |company| "<#{company.trello_url}|#{company.name}>" }
+      links = if companies.count <= TRELLO_LINK_COUNT_THRESHOLD
+        companies.map { |company| "<#{company.trello_url}|#{company.name}>" }
+      else
+        companies.map(&:name)
+      end
       message = "The following companies applied and are waiting to hear back from us!\n#{links.join(', ')}"
       team.notify! message
     end
