2ce5ba858f823ecbdd399354abe8a4b1d21e7f14
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-08 02:31:49 +0100

    Deliver mail synchronously.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,12 +8,12 @@ class ApplicationMailer < ActionMailer::Base
     raise NoMethodError unless respond_to? emailer
     if users.is_a?(Team)
       mail = public_send(emailer, users.email_list, *args)
-      mail.deliver_later
+      mail.deliver_now
       users.notify! mail.text_part.body.decoded
     else
       users = Array.wrap(users)
       mail = public_send(emailer, users.map(&:email), *args)
-      mail.deliver_later
+      mail.deliver_now
       users.each { |user| user.send! mail.text_part.body.decoded }
     end
   end
