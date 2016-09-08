755565de74a561f1d5ca704339dab6260a4ba7d8
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-08 02:35:46 +0100

    Add team name to email subjects.

--- app/mailers/vote_mailer.rb
+++ app/mailers/vote_mailer.rb
@@ -1,21 +1,21 @@
 class VoteMailer < ApplicationMailer
   def upcoming_pitch_email(to, company)
     @company = company
-    mail to: to, subject: "#{SUBJECT_HEADER} #{company.name} Upcoming Pitch"
+    mail to: to, subject: "#{SUBJECT_HEADER} [#{company.team.name.titleize}] #{company.name} Upcoming Pitch"
   end
 
   def funding_decision_email(to, company)
     @company = company
     @stats = company.stats
     @yes_percent = company.votes.final.count > 0 ? @stats[:yes_votes].to_f / company.votes.final.count : 0
-    mail to: to, subject: "#{SUBJECT_HEADER} #{company.name} Funding Decision"
+    mail to: to, subject: "#{SUBJECT_HEADER} [#{company.team.name.titleize}] #{company.name} Funding Decision"
   end
 
   def vote_warning_email(to, company, time_remaining)
     @company = company
     @deadline = Time.now + time_remaining.seconds
     @late = @deadline < Time.now
-    mail to: to, subject: "#{SUBJECT_HEADER} #{company.name} Vote Needed"
+    mail to: to, subject: "#{SUBJECT_HEADER} [#{company.team.name.titleize}] #{company.name} Vote Needed"
   end
 
   def vote_warning_team_email(to, missing_users, company, time_remaining)
@@ -23,6 +23,6 @@ class VoteMailer < ApplicationMailer
     @company = company
     @deadline = Time.now + time_remaining.seconds
     @late = @deadline < Time.now
-    mail to: to, subject: "#{SUBJECT_HEADER} #{company.name} Votes Needed"
+    mail to: to, subject: "#{SUBJECT_HEADER} [#{company.team.name.titleize}] #{company.name} Votes Needed"
   end
 end
