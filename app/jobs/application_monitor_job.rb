91ecd771241fea593e3c0ad3bad8a10c82293a6d
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-10 01:07:03 +0100

    Always send Trello url.

--- app/jobs/application_monitor_job.rb
+++ app/jobs/application_monitor_job.rb
@@ -1,17 +1,11 @@
 class ApplicationMonitorJob < ActiveJob::Base
-  TRELLO_LINK_COUNT_THRESHOLD = 3
-
   queue_as :default
 
   def perform
     Team.for_each do |team|
       companies = team.lists.application.companies
       next if companies.blank?
-      links = if companies.count <= TRELLO_LINK_COUNT_THRESHOLD
-        companies.map { |company| "<#{company.trello_url}|#{company.name}>" }
-      else
-        companies.map(&:name)
-      end
+      links = companies.map { |company| "<#{company.trello_url}|#{company.name}>" }
       message = "The following companies applied and are waiting to hear back from us!" +
         " If you're already talking with one, please move it to the 'Allocated Point Partner' column." +
         "\n#{links.join(', ')}"
