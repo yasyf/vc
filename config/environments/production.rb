eccbc7e5e9045a1dca2c4d19211196ca048ca768
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-13 23:27:38 -0400

    Add default_url_options to production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
