444711d67c3d2e2fb69c67d90cd79dd96ec5c8ae
Yasyf Mohamedali <yasyfm@gmail.com>
2016-09-14 20:59:34 -0400

    Fix email syntax errors. Fixes #24.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,20 +1,20 @@
 class ApplicationMailer < ActionMailer::Base
   SUBJECT_HEADER = '[DRFBot]'
 
-  default from: "DRFBot<#{ENV['GMAIL_USERNAME']}>"
+  default from: "DRFBot <#{ENV['GMAIL_USERNAME']}>"
   layout 'mailer'
 
   def self.email_and_slack!(emailer, users, *args, cc_all: false)
     raise NoMethodError unless respond_to? emailer
     if users.is_a?(Team)
-      mail = public_send(emailer, "#{users.name.titleize} Team<#{users.email_list}>", *args)
-      mail.cc = "All Teams<#{ENV['ALL_TEAMS_MAILING_LIST']}>" if cc_all
+      mail = public_send(emailer, "#{users.name.titleize} Team <#{users.email_list}>", *args)
+      mail.cc = "All Teams <#{ENV['ALL_TEAMS_MAILING_LIST']}>" if cc_all
       mail.deliver_now
       users.notify! mail.text_part.body.decoded
     else
       users = Array.wrap(users)
       mail = public_send(emailer, users.map(&:email), *args)
-      mail.cc = "All Teams<#{ENV['ALL_TEAMS_MAILING_LIST']}>" if cc_all
+      mail.cc = "All Teams <#{ENV['ALL_TEAMS_MAILING_LIST']}>" if cc_all
       mail.deliver_now
       users.each { |user| user.send! mail.text_part.body.decoded }
     end
