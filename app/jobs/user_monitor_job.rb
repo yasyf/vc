bf2c9a5b81964ad9ac1670279104dcda3406ad7e
Yasyf Mohamedali <yasyfm@gmail.com>
2017-04-11 05:06:13 -0400

    Add voting threshold to UserMonitorJob.

--- app/jobs/user_monitor_job.rb
+++ app/jobs/user_monitor_job.rb
@@ -1,5 +1,6 @@
 class UserMonitorJob < ActiveJob::Base
   INACTIVE_THRESHOLD = 1.month
+  VOTE_THRESHOLD = 2.weeks
 
   queue_as :default
 
@@ -8,6 +9,15 @@ class UserMonitorJob < ActiveJob::Base
       User.active(team).where('logged_in_at < ?', INACTIVE_THRESHOLD.ago).each do |user|
         user.toggle_active! user.logged_in_at
       end
+      User
+        .active(team)
+        .joins(:votes)
+        .where('votes.created_at = (SELECT MAX(votes.created_at) FROM votes WHERE votes.user_id = users.id)')
+        .where('votes.created_at < ?', VOTE_THRESHOLD.ago)
+        .select('users.*, votes.created_at as last_voted_on')
+        .each do |user|
+          user.toggle_active! user.last_voted_on
+        end
     end
   end
 end
