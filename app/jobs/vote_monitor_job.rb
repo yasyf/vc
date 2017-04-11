269f0cb1fb8196691883364347454f93d0a8bedc
Yasyf Mohamedali <yasyfm@gmail.com>
2017-04-11 03:17:36 -0400

    Check pitch_on in correct time zone.

--- app/jobs/vote_monitor_job.rb
+++ app/jobs/vote_monitor_job.rb
@@ -4,18 +4,20 @@ class VoteMonitorJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    pending = Company.where('pitch_on <= ?', Date.today).undecided
-    pending.each do |company|
-      next unless company.votes.present?
-      next unless company.quorum?
-      time_remaining = company.team.datetime_now - company.deadline.to_datetime
-      if company.missing_vote_users.count == 0
-        company.decide!
-        company.notify_team!
-        company.move_to_post_pitch_list!
-      elsif time_remaining <= REMAINING_THRESHOLD && time_remaining >= -REMAINING_THRESHOLD
-        warned = company.warn_team! company.missing_vote_users, time_remaining
-        company.missing_votes.each { |vote| vote.warn!(time_remaining) } if warned
+    Team.for_each do |team|
+      pending = Company.where('pitch_on <= ?', Date.today.in_time_zone(team.time_zone)).undecided
+      pending.each do |company|
+        next unless company.votes.present?
+        next unless company.quorum?
+        time_remaining = team.datetime_now - company.deadline.to_datetime
+        if company.missing_vote_users.count == 0
+          company.decide!
+          company.notify_team!
+          company.move_to_post_pitch_list!
+        elsif time_remaining <= REMAINING_THRESHOLD && time_remaining >= -REMAINING_THRESHOLD
+          warned = company.warn_team! company.missing_vote_users, time_remaining
+          company.missing_votes.each { |vote| vote.warn!(time_remaining) } if warned
+        end
       end
     end
   end
