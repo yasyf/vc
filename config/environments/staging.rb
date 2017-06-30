39610bfc0faee28558a8c8f1848eb325e534ff7d
Yasyf Mohamedali <yasyfm@gmail.com>
2017-06-30 15:52:31 -0700

    Fix staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,30 +1,14 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.cache_classes = true
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-
-  # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
-    config.cache_store = :file_store, Rails.root.join('tmp', 'cache')
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
+  config.cache_store = :null_store
 
   # Care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
@@ -48,10 +32,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
