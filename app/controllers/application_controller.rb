06a882817b5e662b139072c16d993987fbd98b42
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-05 12:12:03 -0700

    Fix merge.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_raven_context
-    Raven.user_context(id: current_user.try(:id))
+    Raven.user_context(id: current_internal_user.try(:id))
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 
