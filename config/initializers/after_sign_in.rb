41af4d6aa9d87139f3545154e5471917ff1b5404
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-18 06:09:46 -0700

    Fixes #329.

--- config/initializers/after_sign_in.rb
+++ config/initializers/after_sign_in.rb
@@ -1,5 +1,5 @@
 Warden::Manager.after_set_user do |user, auth, opts|
-  return unless user.respond_to?(:logged_in_at)
+  next unless user.respond_to?(:logged_in_at)
   if user.logged_in_at.blank? || user.logged_in_at < 1.day.ago
     user.update! logged_in_at: Time.now
   end
