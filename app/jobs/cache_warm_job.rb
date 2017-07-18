a7ae51266944199bb2ed3380e584b86556e54e6c
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-18 05:14:59 -0700

    Fixes #318.

--- app/jobs/cache_warm_job.rb
+++ app/jobs/cache_warm_job.rb
@@ -5,7 +5,7 @@ class CacheWarmJob < ActiveJob::Base
     Company.includes(:team, :users, :competitors).all.each do |company|
       company.send(:crunchbase_org, 5)
       begin
-        %w(quorum? funded? stats partner_names as_json).each do |method|
+        %w(funded? stats partner_names as_json).each do |method|
           company.public_send(method)
         end
       rescue Trello::Error => e
