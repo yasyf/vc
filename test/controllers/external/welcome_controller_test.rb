fd0c11f6201515da506112189cbd9193a418173c
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-18 03:47:45 -0700

    Fix redirection test.

--- test/controllers/external/welcome_controller_test.rb
+++ test/controllers/external/welcome_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class External::WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get external_root_url
-    assert_response :success
+    assert_redirected_to external_vcfinder_root_url
   end
 
 end
