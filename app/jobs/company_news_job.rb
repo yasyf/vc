b32e0f8e06dbcd9326f30c0f570739ec52bc5ade
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-20 10:39:22 -0700

    More N+1 queries.

--- app/jobs/company_news_job.rb
+++ app/jobs/company_news_job.rb
@@ -2,7 +2,7 @@ class CompanyNewsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Company.portfolio.map(&:tweeter).compact.each do |tweeter|
+    Company.includes(tweeter: :tweets).portfolio.map(&:tweeter).compact.each do |tweeter|
       tweet = tweeter.newsworthy_tweets.last
       next unless tweet.present? && !tweet.shared?
       tweet.share!
--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -5,6 +5,7 @@ class Tweeter < ApplicationRecord
   NEWSWORTHY_MIN = 10
 
   belongs_to :company
+  has_many :tweets
   validates :username, presence: true, uniqueness: true
 
   def newsworthy_tweets(n = 5)
