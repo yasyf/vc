1daff4a0533d63c712f0c3b887b0080180a1e5f5
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-20 10:10:19 -0700

    Closes #355.

--- app/jobs/card_monitor_job.rb
+++ app/jobs/card_monitor_job.rb
@@ -8,7 +8,7 @@ class CardMonitorJob < ActiveJob::Base
         if move_event && move_event.updated_at < 1.week.ago
           last_date = (move_event.data.last['date'] || move_event.updated_at).to_date
           move_event.touch
-          message = "<#{company.trello_url}|#{company.name}> has been stuck in #{company.list.name} since #{last_date.to_s(:long)}!"
+          message = "<#{company.card.trello_url}|#{company.name}> has been stuck in #{company.card.list.name} since #{last_date.to_s(:long)}!"
           company.users.each do |user|
             user.send! message
           end
