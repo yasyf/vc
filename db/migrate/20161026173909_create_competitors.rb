70e0b789aa60036a04ee3bdcaf2c4560a8054e30
Yasyf Mohamedali <yasyfm@gmail.com>
2017-07-24 18:12:54 -0700

    Ammend historic migration.

--- db/migrate/20161026173909_create_competitors.rb
+++ db/migrate/20161026173909_create_competitors.rb
@@ -8,6 +8,5 @@ class CreateCompetitors < ActiveRecord::Migration[5.0]
     end
     add_index :competitors, :name, unique: true
     add_index :competitors, :crunchbase_id, unique: true
-    Competitor::COMPETITORS.keys.each { |name| Competitor.create_from_name!(name) }
   end
 end
