006abbad687b856a0535f38f4a574349cfb5f03c
Yasyf Mohamedali <yasyfm@gmail.com>
2017-09-06 19:25:32 -0700

    Discard when not found.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,5 @@
 class ApplicationJob < ActiveJob::Base
   retry_on Net::OpenTimeout, wait: :exponentially_longer, attempts: 10
   retry_on Http::AngelList::Errors::RateLimited, wait: :exponentially_longer, queue: :low, attempts: 10
+  discard_on ActiveRecord::RecordNotFound
 end
