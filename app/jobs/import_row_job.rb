6afee06fd1c944255788c5a93600936e69ffedca
Yasyf Mohamedali <yasyfm@gmail.com>
2017-09-13 12:55:25 -0700

    Ignore record errors when importing emails.

--- app/jobs/import_row_job.rb
+++ app/jobs/import_row_job.rb
@@ -1,11 +1,13 @@
 class ImportRowJob < ApplicationJob
+  include Concerns::Ignorable
+
   queue_as :default
 
   def perform(name, parsed)
     klass = name.constantize
     unless klass.process!(parsed) == false
       Rails.logger.info parsed
-      klass.import! parsed
+      ignore_record_errors { klass.import! parsed }
     end
   end
 end
