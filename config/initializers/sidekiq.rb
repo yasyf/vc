655a8a454209f6ab49a1da26d63f6d61a4ce631a
Yasyf Mohamedali <yasyfm@gmail.com>
2017-09-15 04:14:10 -0700

    Remove namespaces for Sidekiq.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@ CLIENT_SIZE = 5
 CONNECTION_LIMIT = 40 - CLIENT_SIZE - 2
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: CLIENT_SIZE, namespace: 'sidekiq' }
+  config.redis = { size: CLIENT_SIZE }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'sidekiq', size: CONNECTION_LIMIT - (ENV['WEB_CONCURRENCY'].to_i * ENV['MAX_THREADS'].to_i) }
+  config.redis = { size: CONNECTION_LIMIT - (ENV['WEB_CONCURRENCY'].to_i * ENV['MAX_THREADS'].to_i) }
 end
 
 Sidekiq.default_worker_options = {
