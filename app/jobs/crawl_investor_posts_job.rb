c19d113052a09900641b89adec37a4284eb7c10e
Yasyf Mohamedali <yasyfm@gmail.com>
2017-09-19 03:08:31 -0700

    Fix typo.

--- app/jobs/crawl_investor_posts_job.rb
+++ app/jobs/crawl_investor_posts_job.rb
@@ -2,7 +2,8 @@ class CrawlInvestorPostsJob < ApplicationJob
   queue_as :default
 
   def perform(investor_id)
+    investor = Investor.find(investor_id)
     return unless investor.blog_url.present?
-    Investor.find(investor_id).crawl_posts!
+    investor.crawl_posts!
   end
 end
