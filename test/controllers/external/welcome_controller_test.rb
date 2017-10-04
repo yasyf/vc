d83d5504543af9ffb15741ed2df4f2bfaa7bd28f
Yasyf Mohamedali <yasyfm@gmail.com>
2017-10-04 15:33:18 -0700

    Fix test.

--- test/controllers/external/welcome_controller_test.rb
+++ test/controllers/external/welcome_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class External::WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get external_root_url
-    assert_redirected_to external_vcfinder_root_url
+    assert_redirected_to external_vcwiz_root_url
   end
 
 end
