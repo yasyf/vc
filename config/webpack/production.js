f9ace0086a1bbc7b6294597b42be12f1d968ba8f
Yasyf Mohamedali <yasyfm@gmail.com>
2017-10-21 03:47:30 -0700

    Update webpack configs.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const ClosureCompilerPlugin = require('webpack-closure-compiler');
 const sharedConfig = require('./shared.js');
 
 module.exports = merge(sharedConfig, {
@@ -16,27 +17,13 @@ module.exports = merge(sharedConfig, {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      sourceMap: true,
-      compress: {
-        warnings: false,
-        unused: true,
-        dead_code: true,
-        drop_debugger: true,
-        drop_console: true,
-        conditionals: true,
-        comparisons: true,
-        sequences: true,
-        booleans: true
+    new ClosureCompilerPlugin({
+      compiler: {
+        compilation_level: 'ADVANCED',
       },
-      output: {
-        comments: false
-      },
-      exclude: [/\.min\.js$/gi]
+      concurrency: 3,
+      exclude: [/\.min\.js$/gi],
     }),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.IgnorePlugin(/^\.\/languages$/, /numbro$/),
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -38,6 +38,8 @@ module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
     new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.IgnorePlugin(/^\.\/languages$/, /numbro$/),
     new ManifestPlugin({
       publicPath: output.publicPath,
       writeToFileEmit: true
