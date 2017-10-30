d59004d0b1869ee6a4a925e0d80ea3aa83ffc3d0
Yasyf Mohamedali <yasyfm@gmail.com>
2017-10-29 19:21:08 -0700

    Remove redundant test.

--- test/controllers/external/welcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class External::WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get external_root_url
-    assert_redirected_to external_vcwiz_root_url
-  end
-
-end
