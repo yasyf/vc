0352d40cd2d8181472e1e5470d37a0dfbdd24003
Yasyf Mohamedali <yasyfm@gmail.com>
2017-11-23 11:03:44 -0800

    Add release script.

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+fetch_site() {
+  URL="https://$MARKETING_DOMAIN"
+  echo "Fetching $URL"
+  until $(curl --output /dev/null --silent --head --fail $URL); do
+    echo "Waiting..."
+    sleep 5
+  done
+  echo "Done!"
+}
+
+rake db:migrate
+
+for run in {1..3}; do
+  sleep 5
+  echo "Run $run"
+  fetch_site
+done
