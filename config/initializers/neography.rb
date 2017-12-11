a038da401decb63277a4aa59d6d906a56fbefc26
Yasyf Mohamedali <yasyfm@gmail.com>
2017-12-11 01:00:25 -0800

    Only load Neography on vcwiz.

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -1,18 +1,20 @@
-if Rails.env.production?
-  uri = URI.parse(ENV['GRAPHENEDB_URL'])
-  Neography.configure do |conf|
-    conf.server = uri.host
-    conf.port = uri.port
-    conf.protocol = "#{uri.scheme}://"
-    conf.authentication = 'basic'
-    conf.username = uri.user
-    conf.password = uri.password
+if Rails.application.vcwiz?
+  if Rails.env.production?
+    uri = URI.parse(ENV['GRAPHENEDB_URL'])
+    Neography.configure do |conf|
+      conf.server = uri.host
+      conf.port = uri.port
+      conf.protocol = "#{uri.scheme}://"
+      conf.authentication = 'basic'
+      conf.username = uri.user
+      conf.password = uri.password
+    end
+  else
+    Neography.configure do |conf|
+      conf.username = 'neo4j'
+      conf.password = ENV['NEO4J_PASSWORD']
+    end
   end
-else
-  Neography.configure do |conf|
-    conf.username = 'neo4j'
-    conf.password = ENV['NEO4J_PASSWORD']
-  end
-end
 
-Graph.init! if Rails.application.vcwiz?
+  Graph.init!
+end
