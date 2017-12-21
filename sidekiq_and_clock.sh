8aa9355bbc5be979dbc54b34e934654373668ba2
Yasyf Mohamedali <yasyfm@gmail.com>
2017-12-21 15:38:00 -0800

    Proper signal forwarding.

--- sidekiq_and_clock.sh
+++ sidekiq_and_clock.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+bundle exec sidekiq -C config/sidekiq.yml -t 25 &
 if [ -n "$RUN_CLOCK" ]; then
   bundle exec zhong config/zhong.rb &
 fi
-bundle exec sidekiq -C config/sidekiq.yml -t 25
+
 wait
