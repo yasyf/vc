945152cc15e4082a2d838ace199ee3c22d6fe94e
Yasyf Mohamedali <yasyfm@gmail.com>
2017-12-21 16:23:29 -0800

    Fix fetching role from crunchbase.

--- lib/http/crunchbase/person.rb
+++ lib/http/crunchbase/person.rb
@@ -46,9 +46,9 @@ module Http::Crunchbase
     end
 
     def affiliation
-      return nil unless (org = response.primary_affiliation&.organization).present?
+      return nil unless (aff = response.primary_affiliation).present?
       @affiliation ||= begin
-        OpenStruct.new({role: org.title, name: org.name, permalink: org.permalink})
+        OpenStruct.new({role: aff.title, name: aff.organization.name, permalink: aff.organization.permalink})
       end
     end
 
