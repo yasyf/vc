4dd30eee42c5bd8ba57c821243bee8cb281d8106
Yasyf Mohamedali <yasyfm@gmail.com>
2017-12-27 17:15:57 -0800

    Fix updating investor stage from standalone page.

--- app/javascript/components/external/vcwiz/investor_page/investor_page.jsx
+++ app/javascript/components/external/vcwiz/investor_page/investor_page.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import VCWiz from '../vcwiz';
 import ModalPage from '../global/shared/modal_page';
 import PartnerTab from '../global/competitors/partner_tab';
-import { TargetInvestorsPath } from '../global/constants.js.erb';
+import { InvestorsPath } from '../global/constants.js.erb';
 import Actions from '../global/actions';
 import {ffetch} from '../global/utils';
 import Lists from '../discover/lists';
 
 export default class InvestorPage extends React.Component {
   onTrackChange = update => {
-    ffetch(TargetInvestorsPath.id(this.props.item.id), 'PATCH', {target_investor: {stage: update.track.value}}).then(() => {
+    ffetch(InvestorsPath.id(this.props.item.id), 'PATCH', {investor: {stage: update.track.value}}).then(() => {
       Actions.trigger('refreshFounder');
     });
   };
