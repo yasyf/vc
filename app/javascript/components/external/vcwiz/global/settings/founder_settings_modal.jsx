fa2f1b460444e7079dbcf5d777066578fe49cec5
Yasyf Mohamedali <yasyfm@gmail.com>
2018-01-03 17:25:08 -0800

    Fix bug.

--- app/javascript/components/external/vcwiz/global/settings/founder_settings_modal.jsx
+++ app/javascript/components/external/vcwiz/global/settings/founder_settings_modal.jsx
@@ -17,7 +17,10 @@ export default class FounderSettingsModal extends SettingsBase {
 
   constructor(props) {
     super(props);
-    this.state = FounderSettingsModal.stateFromFounder(Store.get('founder', {}));
+    this.state = {
+      ...this.state,
+      ...FounderSettingsModal.stateFromFounder(Store.get('founder', {})),
+    };
   }
 
   componentWillMount() {
