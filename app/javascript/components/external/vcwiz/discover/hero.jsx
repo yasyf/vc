dd3b6d6b23d7c9473d1bc6d63816d00ecadd00e8
Yasyf Mohamedali <yasyfm@gmail.com>
2018-01-05 06:18:35 -0800

    Revert "Remove useless prop."
    This reverts commit 94e98bb507698d387381687a3e5daaa128dcaf73.

--- app/javascript/components/external/vcwiz/discover/hero.jsx
+++ app/javascript/components/external/vcwiz/discover/hero.jsx
@@ -52,6 +52,7 @@ export default class Hero extends React.Component {
           industryLimit={2}
           overflowY="hidden"
           applySuggestions={true}
+          shortLink={true}
           render={(header, body) => (
             <div>
               {header}
