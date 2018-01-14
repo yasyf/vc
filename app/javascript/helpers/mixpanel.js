fe5c18a58f11ed0903f7eccccbf8f22c2868e267
Yasyf Mohamedali <yasyfm@gmail.com>
2018-01-14 12:28:39 -0500

    More mixpanel

--- app/javascript/helpers/mixpanel.js
+++ app/javascript/helpers/mixpanel.js
@@ -1,2 +1,8 @@
 mixpanel.identify(gon.userContext.id);
 mixpanel.people.set(gon.userContext);
+
+if (window.LogRocket) {
+  LogRocket.getSessionURL(function (sessionURL) {
+    mixpanel.track('LogRocket', { sessionURL: sessionURL });
+  });
+}
