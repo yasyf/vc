67fc338c05f9f371b0207c67aa430c7ab79dac38
Yasyf Mohamedali <yasyfm@gmail.com>
2018-01-14 12:53:02 -0500

    Fix bug and add unsub link.

--- app/jobs/founder_summary_job.rb
+++ app/jobs/founder_summary_job.rb
@@ -2,7 +2,7 @@ class FounderSummaryJob < ApplicationJob
   queue_as :long
 
   def perform
-    Founder.active.select('id').find_each do |founder|
+    Founder.active.find_each do |founder|
       next if founder.unsubscribed?
       SummaryMailer.weekly_founder_email(founder).deliver_later
     end
