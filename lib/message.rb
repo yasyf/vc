b734a311fae666fb78ec632f397a65ecb341b497
Yasyf Mohamedali <yasyfm@gmail.com>
2018-01-24 18:36:22 -0600

    Fix nil error.

--- lib/message.rb
+++ lib/message.rb
@@ -198,7 +198,7 @@ class Message
        ).any? { |h| headers.key?(h) } ||
       %w(
         feedblitz
-      ).any? { |s| header('Return-Path').include?(s) || header('Sender').include?(s) } ||
+      ).any? { |s| header('Return-Path')&.include?(s) || header('Sender')&.include?(s) } ||
       recipients.push(from).any? do |a|
         (a.local.present? && (%w(noreply no-reply do-not-reply daemon notification support orders team help info admin master hello).any? { |s| a.local.downcase.include?(s) } || a.local.include?('+'))) ||
         (a.name.present? && (['mail delivery', 'support', 'team', 'subsystem', 'accounting', 'payroll', 'admin', 'clara thompson'].any? { |s| a.name.downcase.include?(s) }))
