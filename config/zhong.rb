8cbae54abdf4fdb7f9bd69d7d119d49a9b69240a
Yasyf Mohamedali <yasyfm@gmail.com>
2018-01-28 01:04:55 -0500

    Lists more often.

--- config/zhong.rb
+++ config/zhong.rb
@@ -45,7 +45,7 @@ Zhong.schedule do
       every(1.day, 'clean', at: '01:00') { CompanyCleanJob.perform_later }
       every(12.hours, 'gmail',  skip_first_run: true) { FoundersGmailSyncJob.perform_later }
       every(1.hours, 'investors',  skip_first_run: true) { FindInvestorsJob.perform_later }
-      every(6.hours, 'competitor_lists',  skip_first_run: true) { CompetitorListJob.perform_later }
+      every(3.hours, 'competitor_lists',  skip_first_run: true) { CompetitorListJob.perform_later }
       every(1.hours, 'refresh_views') { RefreshJob.perform_later(only_views: true) }
     end
 
