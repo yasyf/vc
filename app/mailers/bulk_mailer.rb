42c9ff5d436c5f92a7a15ab21905d7b3a8d65820
Yasyf Mohamedali <yasyfm@gmail.com>
2018-05-22 17:41:26 -0400

    Add usefulness survey.

--- app/mailers/bulk_mailer.rb
+++ app/mailers/bulk_mailer.rb
@@ -7,4 +7,11 @@ class BulkMailer < ExternalMailer
     @survey_id = survey_id
     mail to: named_email(founder), subject: subject
   end
+
+  def usefulness_survey_email(founder, survey_id, subject)
+    @founder = founder
+    @url = "https://#{ENV['TYPEFORM_ACCOUNT']}.typeform.com/to/#{survey_id}?#{{email: founder.email}.to_query}"
+    @survey_id = survey_id
+    mail to: named_email(founder), subject: subject
+  end
 end
